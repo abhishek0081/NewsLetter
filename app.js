ae2972febd66fdb270b209aedc64b9d6a6f680e8
Abhishek kumar <rk.abhishek.ram@gmail.com>
2023-02-21 22:04:04 +0530

    Swear it is last initilization

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, './.env') })
 const express = require('express');
 const bodyParser =  require('body-parser');
 const requests = require('request');
 const https = require('https');
-const imp = require( `${__dirname}/.secrets.js`);
+// const imp = require( `${__dirname}/.secrets.js`);
 const app = express();
 
 app.use(bodyParser.urlencoded({extended:true}));
 
-app.use(express.static('public'));
+app.use(express.static('Public'));
 app.get("/",(req,res)=>{
     res.sendFile(`${__dirname}/signUp.html`)
 })
@@ -28,11 +30,11 @@ app.post("/",(req,res)=>{
     const jsonData = JSON.stringify(data);
     const options = {
         method:"POST",
-        auth: `abhishek1:${imp.media}`
+        auth: `abhishek1:${process.env.media}`
     }
 
 
-    const url = `https://us12.api.mailchimp.com/3.0/lists/${imp.audience}/members?skip_merge_validation=${false}`;
+    const url = `https://us12.api.mailchimp.com/3.0/lists/${process.env.audience}/members?skip_merge_validation=${false}`;
 
     const request = https.request(url,options,response=>{
         console.log(response.statusCode);
